a085c86f1ca8b7d8fbc3e576e75342ec274a9b8c
Baluta Cristian <cristi.baluta@gmail.com>
2015-12-04 19:23:00 +0200

    Use curl instead hxssl

--- jit/JiraRequest.hx
+++ jit/JiraRequest.hx
@@ -11,31 +11,61 @@ class JiraRequest {
 		
 		var config = new Config();
 		var baseUrl = config.getJiraUrl();
-		var userPassword64 = encriptedCredentials();
+		var user = config.getJiraUser();
+		var pass = new OSXKeychain().getPasswordForUser(user);
 		
+		//curl -D- -u cbaluta:Cr1st1_Tulc3a -X GET -H "Content-Type: application/json" https://dev.mobility-media.de/jira/rest/api/2/user?username=cbaluta
+		
+		var process = new sys.io.Process("curl", ["-D-", "-u", user+":"+pass, "-X", "GET", "-H", 
+		"\"Content-Type: application/json\"", 
+		baseUrl + "/jira/rest/api/2/issue/" + key + "?fields=key,summary"]);
+		process.exitCode();
+		var result = process.stdout.readAll().toString();
+		if (isValidResponse(result)) {
+			var json = jsonResponse(result);
+			completion(json);
+		} else {
+			completion(null);
+		}
+	}
+	
+	function isValidResponse (response: String): Bool {
+		return response.indexOf("200 OK") != -1;
+	}
+	
+	function jsonResponse (response: String): Dynamic {
+		var components = response.split("Transfer-Encoding: chunked");
+		return haxe.Json.parse(components.pop());
+	}
+	
+	/*public function getIssue_ (key: String, completion: Dynamic->Void) {
+
+		var config = new Config();
+		var baseUrl = config.getJiraUrl();
+		var userPassword64 = encriptedCredentials();
+
 		var r = new haxe.Http( baseUrl + "/jira/rest/api/2/issue/" + key );
 		r.setHeader("Authorization", "Basic " + userPassword64);
 		r.setHeader("Content-Type", "application/json");
 		r.addParameter ("fields", "key,summary");
-		r.onData = function (data: String) { 
+		r.onData = function (data: String) {
 			var json = haxe.Json.parse(data);
-/*			trace(json);*/
 			completion(json);
 		}
 		r.onError = function (data: String) {
 			trace(r);
 		}
 		r.onStatus = function(status) {
-			
+
 		}
 		r.request();
 	}
-	
+
 	function encriptedCredentials(): String {
 		var config = new Config();
 		var user = config.getJiraUser();
 		var pass = new OSXKeychain().getPasswordForUser(user);
 		var userPassword64 = haxe.crypto.Base64.encode( haxe.io.Bytes.ofString (user + ":" + pass) );
 		return userPassword64;
-	}
+	}*/
 }
