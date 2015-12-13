384c80c69dbed16122f4f8174d4c98fd51c4d520
Baluta Cristian <cristi.baluta@gmail.com>
2015-12-13 08:28:15 +0200

    Cleanup

--- jit/command/Jirassic.hx
+++ jit/command/Jirassic.hx
@@ -8,7 +8,6 @@ class Jirassic {
 
 	// osascript -e "tell application \"Jirassic\" to set tasks to \"abcdefghij\""
 	public function logCommit (issueId: String, comments: Array<String>) {
-/*		Sys.println("-----> Log commit to Jirassic");*/
 		var comment = (issueId != null ? (issueId + " ") : "") + comments.join(" ");
 		Sys.command("osascript", ["-e", "tell application \"Jirassic\" to set tasks to \"" + comment +"\""]);
 	}
