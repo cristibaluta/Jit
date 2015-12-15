f0b7027d90e1b6406fcd67a804eee36403cdb0e0
Baluta Cristian <cristi.baluta@gmail.com>
2015-12-15 08:44:23 +0200

    Ignore everything inside []

---
+++ jit/validator/Branch.hx
@@ -0,0 +1,31 @@
+package jit.validator;
+
+class Branch {
+
+	var separator: String;
+	
+	public function new (separator: String) {
+		this.separator = separator;
+	}
+	
+	static public function issueTitleToBranchName (string: String) : String {
+		
+		string = (~/\[[\w ]+\]/g).replace(string, "");// Finds words between []
+		string = (~/[^a-zA-Z\d-]+/g).replace(string, "_");// Finds everything except letters and numbers
+		string = (~/_-_/g).replace(string, "_");
+		string = (~/__/g).replace(string, "_");
+		
+	    return string;
+	}
+	
+	static public function issueIdFromBranchName (branchName: String) : String {
+		
+		var r = ~/[A-a]+-\d+/g;
+		if (r.match ( branchName )) {
+			var issueId = r.matched(0);
+			return issueId;
+		} else {
+			return null;
+		}
+	}
+}
