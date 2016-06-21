736d99515e3294567cf881f1ea0d151b8b5b5c1a
Baluta Cristian <cristi.baluta@gmail.com>
2016-06-21 08:20:55 +0300

    Send the message as json

--- jit/command/Jirassic.hx
+++ jit/command/Jirassic.hx
@@ -1,5 +1,14 @@
 package jit.command;
 
+// enum TaskType {
+// 	case Issue = 0
+// 	case StartDay = 1
+// 	case Scrum = 2
+// 	case Lunch = 3
+// 	case Meeting = 4
+// 	case GitCommit = 5
+// }
+
 class Jirassic {
 	
 	public function new () {
@@ -7,8 +16,9 @@ class Jirassic {
 	}
 
 	// osascript -e "tell application \"Jirassic\" to set tasks to \"abcdefghij\""
-	public function logCommit (issueId: String, comments: Array<String>) {
-		var comment = issueId + "::" + (issueId != null ? (issueId + " ") : "") + comments.join(" ");
+	public function logCommit (taskNumber: String, comments: Array<String>) {
+		var notes = comments.join(" ");
+		var json = "{\"taskType\":\"5\", \"taskNumber\":\"" + taskNumber + "\", \"notes\":\"" + notes + "\"}";
 		Sys.command("osascript", ["-e", "tell application \"Jirassic\" to set tasks to \"" + comment +"\""]);
 	}
 }
