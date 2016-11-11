95c507d592c5f96ca5d83bd2e165ac3f05f75e2e
Cristian Baluta <cristian.baluta@fortech.ro>
2016-11-11 17:47:55 +0200

    Improved config file format
    More flexible for future changes

--- jit/Config.hx
+++ jit/Config.hx
@@ -5,13 +5,19 @@ import jit.security.*;
 
 /**
 Config file is of form:
-	JiraUrl
-	JiraUser
-	BranchWordsSeparator
+	jira_url=url
+	jira_user=user
+	branch_words_separator=_ or -
+	history=branch1;branch2;branch3
 */
 class Config {
 	
-	private var content: Array<String>;
+	private var kJiraUrlKey = "jira_url";
+	private var kJiraUserKey = "jira_user";
+	private var kBranchWordsSeparatorKey = "branch_words_separator";
+	private var kHistoryKey = "history";
+	private var kHistorySeparator = ";";
+	private var values = new Map<String, String>();
 	
 	public function new() {
 		var path = configFilePath();
@@ -19,7 +25,21 @@ class Config {
 			var fout = File.write(path, false);
 				fout.close();
 		}
-		content = File.getContent( path ).split("\n");
+		parse( File.getContent( path));
+	}
+	
+	private function parse (content: String) {
+		var lines = content.split("\n");
+		for (line in lines) {
+			var comps = line.split("=");
+			if (comps[0] == null || comps[0] == "") {
+				continue;
+			}
+			if (comps.length != 2) {
+				continue;
+			}
+			values.set(comps[0], comps[1]);
+		}
 	}
 
 	public function isValid() : Bool {
@@ -27,29 +47,46 @@ class Config {
 	}
 	
 	public function getJiraUrl() : String {
-		return content[0];
+		return values.get(kJiraUrlKey);
 	}
-	
 	public function setJiraUrl (url: String) {
-		content[0] = url;
+		values.set(kJiraUrlKey, url);
 		save();
 	}
 	
 	public function getJiraUser() : String {
-		return content[1];
+		return values.get(kJiraUserKey);
 	}
-	
 	public function setJiraUser (user: String) {
-		content[1] = user;
+		values.set(kJiraUserKey, user);
 		save();
 	}
 	
 	public function getBranchSeparator() : String {
-		return content[2] != null ? content[2] : "_";
+		var separator = values.get(kBranchWordsSeparatorKey);
+		if (separator == null) {
+			separator = "_";
+		}
+		return separator;
 	}
 	
 	public function setBranchSeparator (separator: String) {
-		content[2] = separator;
+		values.set(kBranchWordsSeparatorKey, separator);
+		save();
+	}
+	
+	public function getHistory() : Array<String> {
+		var history = [];
+		var historyString = values.get(kHistoryKey);
+		if (historyString != null) {
+			history = historyString.split(kHistorySeparator);
+		}
+		return history;
+	}
+	public function addToHistory (branchName: String) {
+		var history = getHistory();
+		history.insert(0, branchName);
+		values.set(kHistoryKey, history.join(kHistorySeparator));
 		save();
 	}
 	
@@ -64,6 +101,10 @@ class Config {
 	}
 	
 	function save() {
+		var content = [];
+		for (key in values.keys()) {
+			content.push(key + "=" + values.get(key));
+		}
 		var fout = File.write( configFilePath(), false );
 			fout.writeString( content.join("\n") );
 			fout.close();
