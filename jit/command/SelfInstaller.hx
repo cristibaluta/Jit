8330f953cf30b4187368bcecf704801feb9498bf
Baluta Cristian <cristi.baluta@gmail.com>
2016-12-01 23:03:13 +0200

    Added self installer command

---
+++ jit/command/SelfInstaller.hx
@@ -0,0 +1,15 @@
+package jit.command;
+
+class SelfInstaller extends Installer {
+	
+	override public function run() {
+		
+		var installationPath = binPath() + "/jit";
+		// installationPath = "~/Documents/jitt";
+		Sys.command("curl", ["-o", installationPath, "https://raw.githubusercontent.com/ralcr/Jit/master/build/jit"]);
+		Sys.command("chmod", ["+x", installationPath]);
+		
+		Sys.println( "Installed to: \033[1m"+installationPath+"\033[0m" );
+		Sys.println( "Great, you now have the latest and greatest version" );
+	}
+}
