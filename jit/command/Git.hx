c8d571a83917cda8fe046d31e1c88e393f21f56f
Baluta Cristian <cristi.baluta@gmail.com>
2017-04-07 23:56:43 +0300

    Cleanup

--- jit/command/Git.hx
+++ jit/command/Git.hx
@@ -30,17 +30,15 @@ class Git {
 	
 	public function setUpstream (branchName: String) {
 		// git push --set-upstream origin IOS-2256_T_C_App_Update_Increment_internal_T_C_version
-		Sys.command("git", ["push", "--set-upstream", "origin", branchName]);
-		// Sys.command("git", ["branch", "--set-upstream-to", "origin/" + branchName]);
+		// Sys.command("git", ["push", "--set-upstream", "origin", branchName]);
+		Sys.command("git", ["branch", "--set-upstream-to", "origin/" + branchName]);
 	}
 	
-	public function branchIsUpstream(branchName) : Bool {
-		trace(branchName);
+	public function branchIsUpstream (branchName: String) : Bool {
 		var branches = getLocalBranches(true);
 		for (branch in branches) {
 			trace(branch);
 			if (branch.indexOf(" [origin/" + branchName + "]") > 0) {
-				trace("found");
 				return true;
 			}
 		}
@@ -86,7 +84,7 @@ class Git {
 		return current;
 	}
 	
-	function getLocalBranches(includesTracking: Bool = false) : Array<String> {
+	function getLocalBranches (includesTracking: Bool = false) : Array<String> {
 		var args = ["branch"];
 		if (includesTracking) {
 			args.push("-vv");
